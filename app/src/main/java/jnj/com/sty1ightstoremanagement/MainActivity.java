2075fb1deaaeefed4f3f6abc4a9a3ed6c579cd0b
gjw <lossgate@naver.com>
2017-04-17 23:39:54 +0900

    디비 커넥션 쪽 문제인듯...접속 안되나봄

--- app/src/main/java/jnj/com/sty1ightstoremanagement/MainActivity.java
+++ app/src/main/java/jnj/com/sty1ightstoremanagement/MainActivity.java
@@ -3,6 +3,8 @@ package jnj.com.sty1ightstoremanagement;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.mongodb.Mongo;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // test for github
+        //MongoDBManager.dbManager.ConnectDB();
+        //MongoDBManager.dbManager.test();
+
+
+
+
     }
 }
--- app/src/main/java/jnj/com/sty1ightstoremanagement/MongoDBManager.java
+++ app/src/main/java/jnj/com/sty1ightstoremanagement/MongoDBManager.java
@@ -3,21 +3,66 @@ package jnj.com.sty1ightstoremanagement;
 /**
  * Created by gjw on 2017-04-17.
  */
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.ServerAddress;
 
-public class MongoDBManager {
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
 
-    //커넥트
-    public void ConnectDB()
+import org.bson.Document;
+import java.util.Arrays;
+import com.mongodb.Block;
+
+import com.mongodb.client.MongoCursor;
+import static com.mongodb.client.model.Filters.*;
+import com.mongodb.client.result.DeleteResult;
+import static com.mongodb.client.model.Updates.*;
+import com.mongodb.client.result.UpdateResult;
+import java.util.ArrayList;
+import java.util.List;
+
+public  class MongoDBManager {
+    private static MongoDatabase database;
+    private static String connectString = "mongodb://lossgate:<!jiwoo1021>@sty1ight-shard-00-00-bndb2.mongodb.net:27017,sty1ight-shard-00-01-bndb2.mongodb.net:27017,sty1ight-shard-00-02-bndb2.mongodb.net:27017/<DATABASE>?ssl=true&replicaSet=Sty1ight-shard-0&authSource=admin";
+    private static String DatabaseName = "Sty1ightStoreManagement";
+    public static MongoDBManager dbManager = new MongoDBManager();
+
+        //커넥트
+    public static void ConnectDB()
     {
-        MongoClientURI uri = new MongoClientURI(
-                "mongodb://lossgate:<PASSWORD>@sty1ight-shard-00-00-bndb2.mongodb.net:27017,sty1ight-shard-00-01-bndb2.mongodb.net:27017,sty1ight-shard-00-02-bndb2.mongodb.net:27017/<DATABASE>?ssl=true&replicaSet=Sty1ight-shard-0&authSource=admin");
-
+        MongoClientURI uri = new MongoClientURI(connectString);
         MongoClient mongoClient = new MongoClient(uri);
-        MongoDatabase database = mongoClient.getDatabase("test");
+        database = mongoClient.getDatabase(DatabaseName); // 큰 틀
+    }
+    public static void test()
+    {
+        MongoCollection<Document> collection = database.getCollection("TestCollection"); // 각각의 테이블
+
+        Document doc = new Document("name", "MongoDB")
+                .append("type", "database")
+                .append("count", 1)
+                .append("versions", Arrays.asList("v3.2", "v3.0", "v2.6"))
+                .append("info", new Document("x", 203).append("y", 102));
+        collection.insertOne(doc);
+        SelectAllDocs(collection);
+    }
+
+    //컬렉션 전체 내용 보기
+    public static void SelectAllDocs(MongoCollection<Document> collection)
+    {
+        MongoCursor<Document> cursor = collection.find().iterator();
+        try {
+            while (cursor.hasNext()) {
+                System.out.println(cursor.next().toJson());
+            }
+        } finally {
+            cursor.close();
+        }
     }
 
     //table 생성 코드드
-   public void CreateCollection()
+   public static void CreateCollection()
     {
 
     }
