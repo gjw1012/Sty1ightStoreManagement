746713c28820bebba61745d6256f134a78ab8e74
wodnd23 <wodnd0223@naver.com>
2017-04-17 21:58:48 +0900

    Test code for test github

--- app/src/main/java/jnj/com/sty1ightstoremanagement/MainActivity.java
+++ app/src/main/java/jnj/com/sty1ightstoremanagement/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // test for github //wodnd23
+        // test for github
     }
 }
