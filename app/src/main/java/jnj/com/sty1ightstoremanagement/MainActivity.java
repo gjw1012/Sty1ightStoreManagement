ed5226b1a51f660be6243fa80bd94854f97342c7
gjw <lossgate@naver.com>
2017-04-29 18:55:47 +0900

    dropbox api

--- app/src/main/java/jnj/com/sty1ightstoremanagement/MainActivity.java
+++ app/src/main/java/jnj/com/sty1ightstoremanagement/MainActivity.java
@@ -3,19 +3,18 @@ package jnj.com.sty1ightstoremanagement;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("eee");
         // test for github
-        //MongoDBManager.dbManager.ConnectDB();
-        //MongoDBManager.dbManager.test();
-
-
 
 
     }
